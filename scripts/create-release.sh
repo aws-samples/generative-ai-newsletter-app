655c7499b7b0b08297cc4cec2384d29d0fa62ef5
Addie Rudy <awsrudy@amazon.com>
2024-02-12 13:28:43 -0500

    fix(release): fixed release versioning script

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -3,4 +3,4 @@ VERSION=$(npx git-conventional-commits version)
 npm version ${VERSION} -m "build(release): bump project version to %s"
 npx git-conventional-commits changelog --release ${VERSION} --file 'CHANGELOG.md'
 git commit -am"docs(release): update CHANGELOG.md with ${VERSION}"
-git tag -am"build(release): ${VERSION}" ${VERSION}
+git tag -am"build(release): ${VERSION}" 'v${VERSION}'

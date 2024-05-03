8ae4d0a49ec97f24f060f11d98951ef33086ec11
Addie Rudy <awsrudy@amazon.com>
2024-05-03 16:35:29 -0500

    docs(release): update CHANGELOG.md with 1.1.0

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-VERSION=$(npx git-conventional-commits version)
+# Accept optional argument $1 which is a manual override
+if [ -z "$1" ]; then
+    VERSION=$(npx git-conventional-commits version)
+else
+    VERSION=$1
+fi
 npm version ${VERSION} -m "build(release): bump project version to %s"
 npx git-conventional-commits changelog --release ${VERSION} --file 'CHANGELOG.md'
 git commit -am"docs(release): update CHANGELOG.md with ${VERSION}"

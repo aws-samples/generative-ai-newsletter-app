228a862a259614cced10053bda85f3a525c071dd
Addie Rudy <awsrudy@amazon.com>
2024-06-04 09:59:45 -0400

    WIP: Attempting to convert project to ESM

--- lib/config.ts
+++ lib/config.ts
@@ -7,6 +7,11 @@
 import path from 'path'
 import { type DeployConfig } from '../lib/shared/common/deploy-config'
 import { existsSync, readFileSync } from 'fs'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default function getConfig (pathValue?: string): DeployConfig {
   if (
